635fbdc45c1a281a968ecce1b69341f6d3ce9874
Gustavo Buss <gustavoirio@hotmail.com>
2022-07-27 21:24:21 -0300

    Project created

---
+++ src/test/java/com/buss/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buss.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
