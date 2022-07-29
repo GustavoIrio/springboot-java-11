0a08e0e225aa974fee93d49b557a3d86ba6b1bcd
Gustavo Buss <gustavoirio@hotmail.com>
2022-07-29 00:06:54 -0300

    Order, Instant, ISO 8601 - Part1

--- src/main/java/com/buss/spring/entities/User.java
+++ src/main/java/com/buss/spring/entities/User.java
@@ -1,12 +1,9 @@
 package com.buss.spring.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Column;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -26,6 +23,9 @@ public class User implements Serializable {
     private String phone;
     @Column(name = "password", nullable = false, length = 16)
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -76,6 +76,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
