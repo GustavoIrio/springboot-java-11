8be42d69997aad53cfe572216dd72de7bb4aae8c
Gustavo Buss <gustavoirio@hotmail.com>
2022-07-29 00:41:54 -0300

    OrderStatus enum

--- src/main/java/com/buss/spring/entities/Order.java
+++ src/main/java/com/buss/spring/entities/Order.java
@@ -1,5 +1,6 @@
 package com.buss.spring.entities;
 
+import com.buss.spring.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
@@ -9,7 +10,6 @@ import java.util.Objects;
 @Entity
 @Table(name = "orders")
 public class Order implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -18,6 +18,7 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -25,9 +26,10 @@ public class Order implements Serializable {
     public Order () {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -53,6 +55,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
